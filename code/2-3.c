060cbc614648d24bf0663a0cabac637024b670a6
H1yeee19 <happylacu@gmail.com>
2026-04-09 02:43:46 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-
-void start_strings(char **arr, int size) {
-    char *temp;
-    for (int i = 0; i<size-1; i++) {
-        for (int j = 0; j<size-1-i; j++) {
-            if (strcmp(arr[j], arr[j+1]) > 0) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
 
 int main() {
-    char* words[] = {"apple", "ant", "banana", "cherry"};
-    int size = 4;
+    int a = 10, b = 20, c = 30, d = 40;
+    int *arr[] = {&a, &b, &c, &d};
+    int **pp = arr;
 
-    start_strings(words, size);
+    int **base = arr;
 
-    for (int i=0; i<size; i++) {
-        printf("%s\n", words[i]);
-    }
+    printf("%ld\n", (char *)pp - (char *)base);
+
+    pp = pp+1;
+    printf("%ld\n", (char *)pp - (char *)base);
+
+    pp = pp+1;
+    printf("%ld\n", (char *)pp - (char *)base);
+
+    pp = pp+1;
+    printf("%ld\n", (char *)pp - (char *)base);
+
     return 0;
+
 }
