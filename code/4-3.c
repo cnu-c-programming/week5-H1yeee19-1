960c1742f1f18d8ef80fe26e8c1a2665aa1032eb
H1yeee19 <happylacu@gmail.com>
2026-04-09 03:42:11 +0900

    Update 4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
+
+    scanf("%d", &index);
+
+    if (index < 0 || index >= size) {
+        printf("Error!");
+        return 1;
+    }   
+
+    else {
+        printf("%d", arr[index]);
+    }
+
+    return 0;
+}
